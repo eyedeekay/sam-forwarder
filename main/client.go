32a83eb8db097547a3172cc3b0251b88add12451
idk <problemsolver@openmailbox.org>
2018-08-16 17:32:42 -0400

    Enable UDP client

--- main/client.go
+++ main/client.go
@@ -6,7 +6,13 @@ import "github.com/eyedeekay/sam-forwarder/config"
 
 func clientMode() {
 	if *udpMode {
-		log.Fatal("UDP client mode not implemented yet.")
+		log.Println("Proxying udp", *targetHost+":"+*targetPort, "to", *targetDestination)
+		forwarder, err := i2ptunconf.NewSAMSSUClientForwarderFromConf(config)
+		if err == nil {
+			forwarder.Serve(*targetDestination)
+		} else {
+			log.Println(err.Error())
+		}
 	} else {
 		log.Println("Proxying tcp", *targetHost+":"+*targetPort, "to", *targetDestination)
 		forwarder, err := i2ptunconf.NewSAMClientForwarderFromConf(config)
