569d5efcc3b6e97606a3dd7c99272061026eaa05
idk <problemsolver@openmailbox.org>
2018-09-13 18:19:19 -0400

    snip tunconf into reasonable chunks.

--- config/access.go
+++ config/access.go
@@ -1,5 +1,7 @@
 package i2ptunconf
 
+import "strings"
+
 // GetAccessListType takes an argument and a default. If the argument differs from the
 // default, the argument is always returned. If the argument and default are
 // the same and the key exists, the key is returned. If the key is absent, the
