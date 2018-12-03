fa675eadad935ac7143d5d63c42b3ad193752b8f
idk <problemsolver@openmailbox.org>
2018-12-03 12:17:02 -0500

    improvements to VPN part.

--- csvpn/vpn_test.go
+++ csvpn/vpn_test.go
@@ -20,4 +20,14 @@ func TestVPN(t *testing.T) {
 	} else {
 		t.Fatal(err)
 	}
+	if config, err := i2ptunconf.NewI2PTunConf("../etc/i2pvpn/i2pvpnclient.ini"); err == nil {
+		if vpn, err := NewSAMClientServerVPN(config); err != nil {
+			t.Fatal(err)
+		} else {
+			time.Sleep(time.Duration(30 * time.Second))
+			log.Println(&vpn)
+		}
+	} else {
+		t.Fatal(err)
+	}
 }
