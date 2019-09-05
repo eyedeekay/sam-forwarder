029317222e15f96b68b62973370aee036256f7ba
idk <idk@idk.local>
2019-09-05 17:26:04 -0400

    bandwidth limits for TCP

---
+++ manager/noui.go
@@ -0,0 +1,40 @@
+// +build !static,!nostatic
+
+package sammanager
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func RunUI() {
+}
+
+func (s *app) Serve() bool {
+	log.Println("Starting Tunnels()")
+	for _, element := range s.clientMux.Tunnels() {
+		log.Println("Starting service tunnel", element.ID())
+		go element.Serve()
+	}
+
+	return Exit()
+}
+
+func Exit() bool {
+	Close := false
+	for !Close {
+		time.Sleep(1 * time.Second)
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		go func() {
+			for sig := range c {
+				log.Println(sig)
+
+				Close = true
+			}
+		}()
+	}
+	return false
+}
