3248dd789d00ea2ca7f110609853e2992150a77d
idk <idk@idk.local>
2019-09-11 23:12:33 -0400

    fix go vet for ./interface

--- interface/interface.go
+++ interface/interface.go
@@ -6,28 +6,45 @@ import (
 )
 
 type SAMTunnel interface {
+    // Config returns the appropriate underlying config object all options, or
+    // the common options passed into a compound tunnel.
 	Config() *i2ptunconf.Conf
 	// Tunnel Options
-	GetType() string             // Get the type of the tunnel in use(server, client, http, udp, etc)
-	Print() string               // Print all the tunnel options as a string
-	Props() map[string]string    //Get a full list of tunnel properties as a map for user display/analysis
-	Search(search string) string //Search the Props for a common term
-	Target() string              //The address of the local client or service to forward with a SAM tunnel
-	ID() string                  //The user-chosen tunnel ID
+    // GetType Get the type of the tunnel in use(server, client, http, udp, etc)
+	GetType() string
+    // Print all the tunnel options as a string
+	Print() string
+    // Props Get a full list of tunnel properties as a map for user display/analysis
+	Props() map[string]string
+    //Search the Props for a common term
+	Search(search string) string
+    //Target The address of the local client or service to forward with a SAM tunnel
+	Target() string
+    //ID The user-chosen tunnel name
+	ID() string
 	//Destination() string
 
 	// Key handling
-	Base32() string         // Get the .b32.i2p address of your service
-	Base32Readable() string // Create a more-readable representation of the .b32.i2p address using English words
-	Base64() string         // Get the public base64 address of your I2P service
-	Keys() i2pkeys.I2PKeys  // Get all the parts of the keys to your I2P service
+    // Get the .b32.i2p address of your service
+	Base32() string
+    // Create a more-readable representation of the .b32.i2p address using English words
+	Base32Readable() string
+    // Get the public base64 address of your I2P service
+	Base64() string
+    // Get all the parts of the keys to your I2P service
+	Keys() i2pkeys.I2PKeys
 
 	// Service Management
-	Load() (SAMTunnel, error) // Prepare tunnel keys and tunnel options
-	Serve() error             // Start the tunnel
-	Close() error             // Stop the tunnel and close all connections
-	Cleanup()                 // Stop the tunnel but leave the sockets alone for now
-	Up() bool                 // Return "true" if the tunnel is ready to go up.
+    // Prepare tunnel keys and tunnel options
+	Load() (SAMTunnel, error)
+    // Start the tunnel
+	Serve() error
+    // Stop the tunnel and close all connections
+	Close() error
+    // Stop the tunnel but leave the sockets alone for now
+	Cleanup()
+    // Return "true" if the tunnel is ready to go up.
+	Up() bool
 }
 
 type WebUI interface {
